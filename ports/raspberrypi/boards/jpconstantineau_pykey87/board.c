4537ab2adcb8428511329f047488d53a9f760133
Pierre Constantineau <jpconstantineau@gmail.com>
2021-12-17 22:31:21 -0600

    fix to neopixel reset count

--- ports/raspberrypi/boards/jpconstantineau_pykey87/board.c
+++ ports/raspberrypi/boards/jpconstantineau_pykey87/board.c
@@ -38,7 +38,7 @@ bool board_requests_safe_mode(void) {
 
 void reset_board(void) {
     // turn off any left over LED
-    board_reset_user_neopixels(&pin_GPIO29, 62);
+    board_reset_user_neopixels(&pin_GPIO29, 87);
 }
 
 void board_deinit(void) {
